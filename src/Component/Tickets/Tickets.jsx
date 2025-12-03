75ae0d47892170e82ec68c0d8918223ea94ac2db
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-03 16:38:52 +0600

    Card design is compleat

--- src/Component/Tickets/Tickets.jsx
+++ src/Component/Tickets/Tickets.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import elips from "../../assets/Ellipse.png"
+import calender from "../../assets/calender.png"
 
 const Tickets = () => {
     return (
         <div className='md:w-11/12 mx-auto mt-10 md:mt-20'>
             
-            <div>
-                
+        {/* Ticket */}
+            <div className='grid grid-cols-1 md:grid-cols-12 gap-4'>
+                <div className='col-span-9 mx-2 md:mx-0'>
+                    <h1>Customer Tickets</h1>
+                    <div className='bg-white p-4 shadow-sm rounded-xl'>
+                        <div className='flex justify-between items-center'>
+                            <h1 className='text-xl font-bold'>Login Issues - Can't Access Account</h1>
+                            <button className='flex items-center gap-2 cursor-pointer px-5 py-2 bg-[#b9f8cf] rounded-4xl font-semibold'><img className='w-4 h-4 rounded-5xl' src={elips} alt="" /> Open</button>
+                        </div>
+                        <p className='mt-3 text-gray-500'>Customer is unable to log in to their account. They've tried resetting their password multiple times but still...</p>
+
+                        <div className='flex justify-between items-center mt-3'>
+                            <div className='flex items-center gap-4'>
+                                <p className='text-gray-500 font-semibold'>#1001</p>
+                                <p className='text-red-600'>HIGH PRIORITY</p>
+                            </div>
+                            <div className='flex items-center gap-4'>
+                                <p>John Smith</p>
+                                <div className='flex items-center gap-3'>
+                                    <img src={calender} alt="" />
+                                    <p>1/15/2024</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        {/* Task */}
+                <div className='bg-red-200 col-span-3'>j</div>
             </div>
 
         </div>
