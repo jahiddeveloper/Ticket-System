c818c3252bc2e92db706270b2415df920e223f4f
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-04 07:49:28 +0600

    Daynamic data is compleat

--- src/Component/TicketCard/TicketCard.jsx
+++ src/Component/TicketCard/TicketCard.jsx
@@ -8,28 +8,27 @@ const TicketCard = ({ticket}) => {
       <div className="bg-white p-4 shadow-sm rounded-xl">
               <div className="flex justify-between items-center">
                 <h1 className="text-xl font-bold">
-                  Login Issues - Can't Access Account
+                  {ticket.title}
                 </h1>
                 <button className="flex items-center gap-2 cursor-pointer px-5 py-2 bg-[#b9f8cf] rounded-4xl font-semibold">
                   <img className="w-4 h-4 rounded-5xl" src={elips} alt="" />{" "}
                   Open
                 </button>
               </div>
-              <p className="mt-3 text-gray-500">
-                Customer is unable to log in to their account. They've tried
-                resetting their password multiple times but still...
+              <p className="mt-3 text-gray-500 w-full md:w-125">
+                {ticket.description}
               </p>
 
               <div className="flex justify-between items-center mt-3">
                 <div className="flex items-center gap-4">
-                  <p className="text-gray-500 font-semibold">#1001</p>
-                  <p className="text-red-600">HIGH PRIORITY</p>
+                  <p className="text-gray-500 font-semibold">{ticket.id}</p>
+                  <p className="text-red-600">{ticket.priority}</p>
                 </div>
                 <div className="flex items-center gap-4">
-                  <p>John Smith</p>
+                  <p>{ticket.customer}</p>
                   <div className="flex items-center gap-3">
                     <img src={calender} alt="" />
-                    <p>1/15/2024</p>
+                    <p>{ticket.date}</p>
                   </div>
                 </div>
               </div>
